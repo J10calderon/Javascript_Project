4dd37d2be9375d89e81d1e0f5eda881995729717
Jio Calderon <jcalderon@berkeley.edu>
2017-10-25 16:37:21 -0700

    removed gravity etc. Need to change momentum to movement

--- Project/scripts/main.js
+++ Project/scripts/main.js
@@ -1,12 +1,12 @@
-  var canvas = document.getElementById("myCanvas");
+var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height/2;
 var speedX = 0;
 var speedY = 0;
 var gravity = 0.05;
-var gravitySpeed = 0;
-var momentum = 0;
+var yspeed = 0;
+var xspeed = 0;
 var ballRadius = 10;
 var ballColor = "#0095DD";
 var rightPressed = false;
@@ -14,9 +14,6 @@ var leftPressed = false;
 var upPressed = false;
 var downPressed = false;
 var spacePressed = false;
-var circleBounce = .5;
-var squareBounce = 0;
-var circleMode = true;
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
@@ -31,90 +28,49 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
   }
-function drawSquare() {
-	ctx.beginPath();
-    ctx.rect(x - ballRadius, y - ballRadius, ballRadius * 2, ballRadius * 2);
-    ctx.fillStyle = ballColor;
-    ctx.fill();
-    ctx.closePath();
-}
+
  function draw(){
  	ctx.clearRect(0, 0, canvas.width, canvas.height);
     this.canvas.style.cursor = "none";
 
-    if (spacePressed == true && circleMode == true ){
-        circleMode = false;
-    } else if (spacePressed == true && circleMode == false){
-        circleMode = true;
-    }
- 	swap();
- 	
+ 	drawBall();
     if (rightPressed == true && x < canvas.width-ballRadius-2) {
-    	momentum = 3;
+    	xspeed = 3;
     }
     if (leftPressed == true && x > ballRadius+2) {
-    	momentum = -3;
+    	xspeed = -3;
     }
     if (upPressed == true && y > ballRadius && (y > canvas.height-ballRadius*2 + 8)) {
-    	this.gravitySpeed = -3;
+    	this.yspeed = -3;
     }
     
     if (downPressed == true && y < canvas.height-ballRadius) {
-    	this.gravitySpeed = 3;
+    	this.yspeed = 3;
     }
-    
-    this.gravitySpeed += this.gravity;
-    x += momentum;
-    y += gravitySpeed;
+    x += xspeed;
+    y += yspeed;
 
-    if (circleMode == true){
-        collisionDetectionBounce();
-    } else {
-        collisionDetection();
-    }
 
- }
- function swap(){
- 	if (circleMode == true){
- 		drawBall();
- 	} else {
- 		drawSquare();
- 	}
+    collisionDetection();
+
  }
 
  function collisionDetection(){
  	if (y + speedY >= canvas.height - ballRadius){
  		y = canvas.height - ballRadius;
-        this.gravitySpeed = 0;
+        this.yspeed = 0;
  	}
  	else if (y + speedY <= ballRadius){
- 		this.gravitySpeed = 0;
+ 		this.yspeed = 0;
  	}
  	else if (x + speedX >= canvas.width - ballRadius){
  		speedX = 0;
-        this.momentum = -this.momentum;
+        this.xspeed = -this.xspeed;
  	}
  	else if (x + speedX <= ballRadius){
  		speedX = 0;
-        this.momentum = -this.momentum;
+        this.xspeed = -this.xspeed;
  	}
-
- }
- function collisionDetectionBounce(){
-    if (y + speedY >= canvas.height - ballRadius){
-        y = canvas.height - ballRadius;
-        this.gravitySpeed = -this.gravitySpeed;
-    }
-    else if (y + speedY <= ballRadius){
-        this.gravitySpeed = 0;
-        this.momentum = 0;
-    }
-    else if (x + speedX >= canvas.width - ballRadius){
-        this.momentum = -this.momentum;
-    }
-    else if (x + speedX <= ballRadius){
-        this.momentum = -this.momentum;
-    }
 
  }
 
